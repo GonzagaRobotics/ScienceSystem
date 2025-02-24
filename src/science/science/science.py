da24672e9461917721ace8029a8b255ac4371350
GU Robotics Embedded System <gurobotics1887@gmail.com>
2025-02-23 18:33:25 -0800

    Lucas: I updated the pin values, added the auger, and modified the stepper motor code to get it working as intended

--- src/science/science/science.py
+++ src/science/science/science.py
@@ -9,12 +9,24 @@ FORWARD = 1
 BACKWARD = 0
 
 # Output pins
-AUGER_DRILL_EN = 2
+AUGER_DRILL_EN = 13 
 # The L298N uses two pins to control the direction of the motor
-AUGER_DRILL_DIR1 = 3
-AUGER_DRILL_DIR2 = 4
-AUGER_VERTICAL_STEP = 0
-AUGER_VERTICAL_DIR = 0
+AUGER_DRILL_DIR1 = 19
+AUGER_DRILL_DIR2 = 26
+AUGER_VERTICAL_STEP = 14
+AUGER_VERTICAL_DIR = 15
+
+# Output pins
+ACTUATOR_EN = 16
+# The L298N uses two pins to control the direction of the motor
+ACTUATOR_DIR3 = 20
+ACTUATOR_DIR4 = 21
+
+LIMIT_SWITCH_BTN = 10
+
+
+# Default speed for stepper motors
+AUGER_VERT_SPEED = 0.0
 
 # Linear actuator PWM frequency
 ACTUATOR_FREQUENCY = 1000
@@ -30,6 +42,7 @@ LAST_STEPPER_TICK_TIME = 0
 AUGER_VERTICAL_LAST_VALUE = 0
 # Are we currently stepping?
 AUGER_VERTICAL_ON = 0
+
 
 PI: pigpio.pi
 
@@ -72,6 +85,34 @@ def set_auger_vertical(dir: int):
     AUGER_VERTICAL_ON = dir != 0
     PI.write(AUGER_VERTICAL_DIR, dir > 0)
 
+def set_auger_speed(speed: float):
+    global AUGER_VERT_SPEED
+    
+    AUGER_VERT_SPEED = speed
+
+def move_auger_vertical():
+    global AUGER_VERT_SPEED
+
+    speed = AUGER_VERT_SPEED
+
+    if abs(speed) < 0.1:
+        speed = 0
+
+    # Should be fine, since we're already cutting off everything below 0.1 to exactly 0
+    if (speed == 0):
+        return
+
+    if (PI.read(LIMIT_SWITCH_BTN) == 1 and speed < 0):
+        PI.write(AUGER_VERTICAL_STEP,0);
+        return
+
+    PI.write(AUGER_VERTICAL_DIR, 1 if (speed > 0) else 0);
+    PI.write(AUGER_VERTICAL_STEP,1);
+    # 1000 microseconds
+    time.sleep(1000/1000000.0);
+    PI.write(AUGER_VERTICAL_STEP,0);
+    time.sleep(1000/1000000.0);
+
 
 def set_auger_drill(speed: float):
     if abs(speed) < 0.1:
@@ -83,6 +124,17 @@ def set_auger_drill(speed: float):
     PI.write(AUGER_DRILL_DIR2, speed < 0)
 
     PI.set_PWM_dutycycle(AUGER_DRILL_EN, abs(speed))
+
+def set_actuator(speed: float):
+    if abs(speed) < 0.1:
+        speed = 0
+
+    speed = int(speed * 255)
+
+    PI.write(ACTUATOR_DIR3, speed > 0)
+    PI.write(ACTUATOR_DIR4, speed < 0)
+
+    PI.set_PWM_dutycycle(ACTUATOR_EN, abs(speed))
 
 
 def stepper_tick():
@@ -105,6 +157,7 @@ def stepper_tick():
 
 def shutdown():
     set_auger_drill(0)
+    set_actuator(0)
     set_auger_vertical(0)
 
     PI.stop()
@@ -143,16 +196,21 @@ class Science(Node):
 
     def auger_vertical_callback(self, msg: Float32):
         self.get_logger().info('Auger vertical: %s' % msg.data)
-
+    
+        set_auger_speed(msg.data)
         # set_auger_vertical(msg.data)
+        # move_auger_vertical(msg.data)
 
     def auger_drill_callback(self, msg: Float32):
         self.get_logger().info('Auger drill: %s' % msg.data)
 
-        # set_auger_drill(msg.data)
+        set_auger_drill(msg.data)
+
 
     def auger_actuate_callback(self, msg: Float32):
         self.get_logger().info('Auger actuate: %s' % msg.data)
+
+        set_actuator(msg.data)
 
 
 def main(args=None):
@@ -163,14 +221,19 @@ def main(args=None):
     PI.set_mode(AUGER_DRILL_EN, pigpio.OUTPUT)
     PI.set_mode(AUGER_DRILL_DIR1, pigpio.OUTPUT)
     PI.set_mode(AUGER_DRILL_DIR2, pigpio.OUTPUT)
+
+    PI.set_mode(ACTUATOR_EN, pigpio.OUTPUT)
+    PI.set_mode(ACTUATOR_DIR3, pigpio.OUTPUT)
+    PI.set_mode(ACTUATOR_DIR4, pigpio.OUTPUT)
+
+    PI.set_mode(LIMIT_SWITCH_BTN, pigpio.INPUT)
+
     PI.set_mode(AUGER_VERTICAL_STEP, pigpio.OUTPUT)
     PI.set_mode(AUGER_VERTICAL_DIR, pigpio.OUTPUT)
 
     PI.set_PWM_frequency(AUGER_DRILL_EN, 1000)
     PI.set_PWM_dutycycle(AUGER_DRILL_EN, 0)
 
-    # PI.hardware_PWM(SHOULDER_PWM, ACTUATOR_FREQUENCY, 0)
-    # PI.hardware_PWM(FOREARM_PWM, ACTUATOR_FREQUENCY, 0)
 
     rclpy.init(args=args)
 
@@ -179,7 +242,8 @@ def main(args=None):
     try:
         while rclpy.ok():
             rclpy.spin_once(science, timeout_sec=0)
-            stepper_tick()
+            move_auger_vertical()
+            # stepper_tick()
     except KeyboardInterrupt:
         pass
     finally:
